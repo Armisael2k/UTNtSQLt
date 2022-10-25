f58fc55e9aeb9c48ca02f73c908c548101ff2e7d
rulopez <ruben_guille27@hotmail.com>
2022-10-24 22:18:49 -0700

    Refactor en el script para ejecutar el ejercicio de DELETE y comprobar que el registro fue borrado

--- DELETE/EXEC Person.uspDeleteEmailAddress.sql
+++ DELETE/EXEC Person.uspDeleteEmailAddress.sql
@@ -4,4 +4,12 @@ GO
 --TODO: Cambia el valor de @EmailAddressID para ejecutar el procedimiento almacenado
 DECLARE @EmailAddressID INT = 0
 
+
+--Ver los valores anteriores
+SELECT * FROM Person.EmailAddress WHERE EmailAddressID = @EmailAddressID
+
+--Ejecutar el SP
 EXEC Person.uspDeleteEmailAddress @EmailAddressID = @EmailAddressID
+
+--Comprobar que el registro ya no existe
+SELECT * FROM Person.EmailAddress WHERE EmailAddressID = @EmailAddressID
